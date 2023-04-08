884e821faa9fe3978d4ae95a98049dffe0b6a5cd
Wiktor Mociun <voter101@gmail.com>
2023-04-08 11:11:10 +0200

    Full solution, day 20

--- 2022/day20/src/main.rs
+++ 2022/day20/src/main.rs
@@ -1,43 +1,38 @@
 use std::fs;
 
+fn decode(numbers_input: &Vec<(usize, i64)>, rounds: usize, key: usize) -> i64 {
+    let mut numbers: Vec<(usize, i64)> = numbers_input
+        .clone()
+        .iter_mut()
+        .map(|(index, val)| (index.clone(), val.clone() * key as i64))
+        .collect();
+    let numbers_len = numbers.len();
+
+    for _ in 0..rounds {
+        for index in 0..numbers_len {
+            let current_index = numbers.iter().position(|&e| e.0 == index).unwrap();
+            let potential_index = current_index as i64 + numbers[current_index].1;
+            let new_index = potential_index.rem_euclid(numbers_len as i64 - 1);
+            let val = numbers.remove(current_index as usize);
+            numbers.insert(new_index as usize, val);
+        }
+    }
+
+    let zero = numbers.iter().position(|&e| e.1 == 0).unwrap();
+    return [1000, 2000, 3000]
+        .iter()
+        .map(|shift| numbers[(zero + shift) % numbers_len].1)
+        .sum::<i64>();
+}
+
 fn main() {
     let input = fs::read_to_string("./input.txt").expect("File not loaded");
     let numbers = input
         .lines()
-        .map(|line| line.parse::<i32>().unwrap())
+        .map(|line| line.parse::<i64>().unwrap())
+        .enumerate()
         .collect::<Vec<_>>();
-    let numbers_len = numbers.len();
 
-    let mut numbers_moving = numbers.clone();
-
-    for curr in numbers {
-        if curr != 0 {
-            let index = numbers_moving.iter().position(|&e| e == curr).unwrap() as i32;
-            let potential_index = index + curr;
-            numbers_moving.remove(index as usize);
-            if potential_index == 0 {
-                numbers_moving.push(curr);
-            } else {
-                let new_index_ = potential_index.rem_euclid((numbers_len - 1) as i32);
-                let new_index: usize = new_index_ as usize;
-
-                numbers_moving.insert(new_index, curr);
-            }
-        }
-    }
-
-    let mut numbers_cycle = numbers_moving.iter().cycle();
-    loop {
-        if numbers_cycle.next().unwrap().clone() == 0 {
-            break;
-        }
-    }
-    let result: i32 = [
-        numbers_cycle.nth(999).unwrap().clone(),
-        numbers_cycle.nth(1000).unwrap().clone(),
-        numbers_cycle.nth(1000).unwrap().clone(),
-    ]
-    .iter()
-    .sum();
-    println!("{}", result);
+    println!("{}", decode(&numbers, 1, 1));
+    println!("{}", decode(&numbers, 10, 811589153));
 }
